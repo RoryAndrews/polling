8fdb3d1c67c5bbab6896fcd84321ef6c86af49aa
Rory Andrews <rory00@gmail.com>
2015-10-29 18:02:38 -1000

    Removed autopublish and insecure from meteor.

--- collections/polls.js
+++ collections/polls.js
@@ -2,3 +2,15 @@
  * Created by Rory on 10/29/2015.
  */
 Polls = new Mongo.Collection('polls');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("polls", function () {
+    return Polls.find();
+  });
+}
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("polls");
+}
